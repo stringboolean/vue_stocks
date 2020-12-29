f0dbe8d9083f2422ef4bb3dd6ab027fc2be3df15
Joe Moore <josephmoore.it@gmail.com>
2020-12-29 02:37:13 -0500

    added url root

--- src/main.js
+++ src/main.js
@@ -14,6 +14,8 @@ Vue.config.productionTip = false;
 
 Vue.use(VueRouter);
 
+Vue.http.options.root = "https://josephmoore.herokuapp.com/";
+
 const router = new VueRouter({
   mode: 'history',
   routes
