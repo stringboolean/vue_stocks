eba910b1df03050817477a98caf4dea20c803810
Joe Moore <josephmoore.it@gmail.com>
2020-12-29 02:54:13 -0500

    flailing along

--- src/main.js
+++ src/main.js
@@ -1,5 +1,7 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
+import VueResource from 'vue-resource';
+
 import App from './App.vue';
 import { routes } from './routes';
 import store from './store/store';
@@ -13,6 +15,9 @@ Vue.component('font-awesome-icon', FontAwesomeIcon);
 Vue.config.productionTip = false;
 
 Vue.use(VueRouter);
+Vue.use(VueResource);
+
+Vue.http.options.root = "https://josephmoore.herokuapp.com/";
 
 const router = new VueRouter({
   mode: 'history',
